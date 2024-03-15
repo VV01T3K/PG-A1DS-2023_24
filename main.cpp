ae51e7f42f6574ddd3eedb388663e8870041e204
VV01T3K <wojteks.access@gmail.com>
2024-03-15 08:56:14 +0000

    xd

--- main.cpp
+++ main.cpp
@@ -1,83 +1,119 @@
 #include <iostream>
-#include <stdexcept>
 using namespace std;
 
-class DynamicArray_2D {
-    int **array;
-    int max_row;
-    int max_col;
+class Stack {
+   private:
+    char* stack;
+    int size;
+    int wskaznik = 0;
 
    public:
-    DynamicArray_2D(int row, int col) : max_row(row), max_col(col) {
-        array = new int *[max_row];
-        for (int i = 0; i < max_row; i++) {
-            array[i] = new int[max_col];
-            std::fill_n(array[i], max_col, 0);
-        }
-    }
-    ~DynamicArray_2D() {
-        for (int i = 0; i < max_row; ++i) delete[] array[i];
-        delete[] array;
+    int count = 0;
+    bool overflow = false;
+    Stack(int new_size) {
+        size = new_size;
+        stack = new char[new_size]{0};
     };
-    void print() const {
-        for (int i = 0; i < max_row; i++) {
-            for (int j = 0; j < max_col; j++) {
-                cout << array[i][j] << " ";
-            }
-            cout << "\n";
+    ~Stack() { delete[] stack; };
+    void push(char c) {
+        if (wskaznik + 1 > size) overflow = true;
+        stack[wskaznik++] = c;
+        count++;
+    };
+
+    char pop() {
+        count--;
+        return stack[wskaznik-- - 1];
+    };
+
+    char peek() { return stack[wskaznik - 1]; };
+
+    void print() {
+        for (int i = 0; i < wskaznik; i++) {
+            cout << stack[i] << " ";
         }
-    }
-    int &operator()(int row, int column) {
-        if (row < 0) row += max_row;
-        if (column < 0) column += max_col;
-        if (row < 0 || row >= max_row)
-            throw std::out_of_range("Row out of range");
-        if (column < 0 || column >= max_col)
-            throw std::out_of_range("Column out of range");
-
-        return array[row][column];
-    }
-    int *operator[](int index) {
-        if (index < 0 || index >= max_row)
-            throw std::out_of_range("Index out of range");
-
-        return array[index];
-    }
+        cout << endl;
+    };
 };
+
+char getznak(char c) {
+    switch (c) {
+        case '(':
+            return ')';
+            break;
+        case '{':
+            return '}';
+            break;
+        case '<':
+            return '>';
+            break;
+        case '[':
+            return ']';
+            break;
+        case ')':
+            return '(';
+            break;
+        case '}':
+            return '{';
+            break;
+        case '>':
+            return '<';
+            break;
+        case ']':
+            return '[';
+            break;
+        default:
+            return '0';
+            break;
+    }
+}
+
 int main() {
-    int row, col, distance;
-
-    cin >> row >> col;
-
-    DynamicArray_2D array(row, col);
-
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            cin >> array(i, j);
+    char c;
+    int n;
+    cin >> n;
+    Stack stack(n);
+    bool stop = false;
+    int znak_nr = -1;
+    while (scanf("%c", &c) == 1) {
+        if (stop) break;
+        if (stack.overflow) {
+            cout << "Stack Overflow\n";
+            break;
+        }
+        znak_nr++;
+        switch (c) {
+            case '[':
+            case '<':
+            case '(':
+            case '{':
+                stack.push(c);
+                break;
+            case ']':
+            case '>':
+            case ')':
+            case '}':
+                if (stack.count <= 0) {
+                    cout << "error:" << znak_nr
+                         << ": expected statement before '" << c << "' token\n";
+                    stop = true;
+                    break;
+                } else if (stack.peek() != getznak(c)) {
+                    cout << "error:" << znak_nr << ": expected '"
+                         << getznak(stack.peek()) << "' before '" << c
+                         << "' token\n";
+                    stop = true;
+                    break;
+                }
+                stack.pop();
+                break;
+            default:
+                break;
         }
     }
-    cin >> distance;
-
-    int i;
-
-    for (int d = 0; d < distance; d++) {
-        int tmp = array(0, 0);
-        for (i = 0; i < col - 1; i++) {
-            array(0, i) = array(0, i + 1);
-        }
-        for (i = 0; i < row - 1; i++) {
-            array(i, -1) = array(i + 1, -1);
-        }
-        for (i = col - 1; i > 0; i--) {
-            array(-1, i) = array(-1, i - 1);
-        }
-        for (i = row - 1; i > 0; i--) {
-            array(i, 0) = array(i - 1, 0);
-        }
-        array(1, 0) = tmp;
-    }
-
-    array.print();
-
-    return 0;
+    if (stack.count > 0)
+        cout << "error:" << znak_nr + 2 << ": expected statement before '"
+             << getznak(stack.peek()) << "' token\n";
+    else if (!stack.overflow && !stop)
+        cout << "ok\n";
 }
