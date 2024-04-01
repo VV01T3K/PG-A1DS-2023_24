f7b3c9765bda56053e66edea7ad17f0f85ea4708
VV01T3K <wojteks.access@gmail.com>
2024-04-01 21:21:07 +0000

    semi working

--- Project/main.cpp
+++ Project/main.cpp
@@ -7,54 +7,67 @@ using namespace std;
 int main() {
     // enum State { EMPTY = ' ', RED = 'r', BLUE = 'b' };
 
-    vector<vector<char>> tmp;
-    int size = 0;
-    int width = 0;
-    char c;
-    while (cin.get(c)) {
-        if (c == '.') break;
-        if (c == '-') continue;
-        if (c == ' ') continue;
-        if (c == '<') {
-            width++;
-            cin >> c;
-            if (c == '>') c = 'x';
-            tmp.back().push_back(c);
-        }
-        if (c == '>') continue;
-        if (c == '\n') {
-            // if (width < size) break;
-            size = max(size, width);
-            width = 0;
-            tmp.push_back({});
-            continue;
-        }
-    }
-    for (int i = 0; i < tmp.size(); i++) {
-        for (int j = 0; j < tmp[i].size(); j++) {
-            cout << tmp[i][j];
+    // vector<vector<char>> tmp;
+    // int size = 0;
+    // int width = 0;
+    // char c;
+    // while (cin.get(c)) {
+    //     if (c == '.') break;
+    //     if (c == '-') continue;
+    //     if (c == ' ') continue;
+    //     if (c == '<') {
+    //         width++;
+    //         cin >> c;
+    //         if (c == '>') c = 'x';
+    //         tmp.back().push_back(c);
+    //     }
+    //     if (c == '>') continue;
+    //     if (c == '\n') {
+    //         // if (width < size) break;
+    //         size = max(size, width);
+    //         width = 0;
+    //         tmp.push_back({});
+    //         continue;
+    //     }
+    // }
+    // for (int i = 0; i < tmp.size(); i++) {
+    //     for (int j = 0; j < tmp[i].size(); j++) {
+    //         cout << tmp[i][j];
+    //     }
+    //     cout << endl;
+    // }
+    // HexBoard board(11);
+
+    // size = 3;
+    // board.getHex(0, 0)->symbol = tmp[0][0];
+
+    // board.getHex(0, 1)->symbol = tmp[1][0];
+    // board.getHex(1, 0)->symbol = tmp[1][1];
+
+    // board.getHex(0, 2)->symbol = tmp[2][0];
+    // board.getHex(1, 1)->symbol = tmp[2][1];
+    // board.getHex(2, 0)->symbol = tmp[2][2];
+
+    // board.getHex(1, 2)->symbol = tmp[3][0];
+    // board.getHex(2, 1)->symbol = tmp[3][1];
+
+    // board.getHex(2, 2)->symbol = tmp[4][0];
+
+    int size = 3;
+    int q = 0;
+    int r = 0;
+    for (int i = 0; i < size; i++) {
+        int tmp_q = q;
+        int tmp_r = r;
+        for (int j = 0; j < abs(q - r) + 1; j++) {
+            cout << tmp_q++ << " " << tmp_r-- << ", ";
         }
         cout << endl;
+        r++;
     }
-    HexBoard board(11);
 
-    size = 3;
-    board.getHex(0, 0)->symbol = tmp[0][0];
-
-    board.getHex(0, 1)->symbol = tmp[1][0];
-    board.getHex(1, 0)->symbol = tmp[1][1];
-
-    board.getHex(0, 2)->symbol = tmp[2][0];
-    board.getHex(1, 1)->symbol = tmp[2][1];
-    board.getHex(2, 0)->symbol = tmp[2][2];
-
-    board.getHex(1, 2)->symbol = tmp[3][0];
-    board.getHex(2, 1)->symbol = tmp[3][1];
-
-    board.getHex(2, 2)->symbol = tmp[4][0];
-
-    cout << endl << "==================" << endl;
-    board.print();
+    // cout << endl << "==================" << endl;
+    // board.print();
 
     return 0;
 }
