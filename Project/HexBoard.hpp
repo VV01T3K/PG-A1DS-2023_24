6b70102c3ede0d0b057b06e886aa5665febf5293
VV01T3K <wojteks.access@gmail.com>
2024-04-01 21:54:16 +0000

    git gut great

--- Project/HexBoard.hpp
+++ Project/HexBoard.hpp
@@ -6,16 +6,41 @@
 
 #include "Hex.hpp"
 
+#define MAX_BOARD_SIZE 11
+
 class HexBoard {
+   private:
+    char tmp[MAX_BOARD_SIZE * MAX_BOARD_SIZE];
+
+    void readBoardFromInput() {
+        int index = 0;
+        int width = 0;
+        char c;
+        while (std::cin.get(c)) {
+            if (c == EOF) break;
+            if (c == '-' || c == ' ' || c == '>') continue;
+            if (c == '<') {
+                width++;
+                std::cin >> c;
+                if (c == '>') c = 'x';
+                tmp[index++] = c;
+            }
+            if (c == '\n') {
+                size = std::max(size, width);
+                width = 0;
+            }
+        }
+    }
+
    public:
-    int size;
+    int size = 0;
     std::vector<Hex *> hexes;
 
-    HexBoard(int size) : size(size) {
-        hexes.reserve(size * size);
-        for (int i = 0; i < size * size; i++) {
-            int q = i % size;
-            int r = i / size;
+    HexBoard() {
+        hexes.reserve(MAX_BOARD_SIZE * MAX_BOARD_SIZE);
+        for (int i = 0; i < MAX_BOARD_SIZE * MAX_BOARD_SIZE; i++) {
+            int q = i % MAX_BOARD_SIZE;
+            int r = i / MAX_BOARD_SIZE;
             hexes.push_back(new Hex(q, r, *this));
         }
     }
@@ -43,6 +68,26 @@ class HexBoard {
                 }
             }
             std::cout << std::endl;
+        }
+    }
+
+    void loadBoard() {
+        readBoardFromInput();
+        int index = 0;
+        int q = 0;
+        int r = 0;
+        for (int i = 0; i < 2 * size - 1; i++) {
+            int tmp_q = q;
+            int tmp_r = r;
+            for (int j = 0; j < std::abs(q - r) + 1; j++) {
+                getHex(tmp_q, tmp_r)->symbol = tmp[index++];
+                tmp_q++;
+                tmp_r--;
+            }
+            if (i < size - 1)
+                r++;
+            else
+                q++;
         }
     }
 };
--- Project/main.cpp
+++ Project/main.cpp
@@ -4,45 +4,13 @@
 #include "HexBoard.hpp"
 using namespace std;
 
+#define MAX_BOARD_SIZE 11
+
 int main() {
     // enum State { EMPTY = ' ', RED = 'r', BLUE = 'b' };
-    int index = 0;
-    char tmp[11 * 11];
-    int size = 0;
-    int width = 0;
-    char c;
-    while (cin.get(c)) {
-        if (c == EOF) break;
-        if (c == '-' || c == ' ' || c == '>') continue;
-        if (c == '<') {
-            width++;
-            cin >> c;
-            if (c == '>') c = 'x';
-            tmp[index++] = c;
-        }
-        if (c == '\n') {
-            size = max(size, width);
-            width = 0;
-        }
-    }
-    HexBoard board(11);
-    int q = 0;
-    int r = 0;
-    index = 0;
-    for (int i = 0; i < 2 * size - 1; i++) {
-        int tmp_q = q;
-        int tmp_r = r;
-        for (int j = 0; j < abs(q - r) + 1; j++) {
-            board.getHex(tmp_q, tmp_r)->symbol = tmp[index++];
-            tmp_q++;
-            tmp_r--;
-        }
-        if (i < size - 1)
-            r++;
-        else
-            q++;
-    }
+    HexBoard board;
 
+    board.loadBoard();
     board.print();
 
     return 0;
