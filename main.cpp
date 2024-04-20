c7a2d23b8c909e66dc067e496db477ea1f8e50bb
VV01T3K <wojteks.access@gmail.com>
2024-04-20 23:54:09 +0000

    working but really bad

--- main.cpp
+++ main.cpp
@@ -1,48 +1,81 @@
+#include <forward_list>
 #include <iostream>
+#include <stack>
+#include <utility>
+#include <vector>
 using namespace std;
 
-enum maze {
+enum MAZE {
     WALL = 0,
     FLOOR = 1,
     START = 3,
     CHEESE = 5,
 };
 
-enum direction {
-    SOUTH = 0,
-    EAST = 1,
-    WEST = 2,
-    NORTH = 3,
-};
-
-class Stack {
-   private:
-    int* array;
-    int size = 0;
-    int max_size;
-
+class Point {
    public:
-    Stack(int size) : max_size(size) { array = new int[size]; }
-    ~Stack() { delete[] array; }
-    void push(int element) {
-        if (size < max_size) array[size++] = element;
-    }
-    int pop() { return array[--size]; }
+    int x, y;
+    MAZE type;
+    bool visited = false;
+    Point(int x, int y, int type) : x(x), y(y), type((MAZE)type) {}
+    Point() {}
 };
 
 int main() {
+    system("clear");
     int n, m;
     cin >> n >> m;
-    Stack stack(n * m);
-    int* maze = new int[n * m];
-    for (int i = 0; i < n * m; i++) cin >> maze[i];
+    vector<vector<Point>> maze(n, vector<Point>(m));
+    pair<int, int> start;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++) {
+            int tmp;
+            cin >> tmp;
+            maze[i][j] = Point(i, j, tmp);
+            if (tmp == START) start = {i, j};
+        }
 
-    // print maze
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) cout << maze[i * m + j] << " ";
-        cout << endl;
+    std::forward_list<pair<int, int>> path;
+    stack<pair<int, int>> stack;
+    maze[start.first][start.second].visited = true;
+    stack.push(start);
+
+    while (!stack.empty()) {
+        pair<int, int> current = stack.top();
+        path.push_front(current);
+        stack.pop();
+        if (maze[current.first][current.second].type == CHEESE) break;
+        for (int i = 0; i < 4; i++) {
+            int x = current.first, y = current.second;
+            switch (i) {
+                case 0:
+                    x--;
+                    break;
+                case 1:
+                    y--;
+                    break;
+                case 2:
+                    y++;
+                    break;
+                case 3:
+                    x++;
+                    break;
+            }
+            if (x < 0 || x >= n || y < 0 || y >= m) continue;
+            if (maze[x][y].type == WALL || maze[x][y].visited) continue;
+            maze[x][y].visited = true;
+            stack.push({x, y});
+        }
     }
 
-    delete[] maze;
-    return 0;
+    // n x m array filed with 0
+    vector<vector<int>> path_map(n, vector<int>(m, 0));
+    for (auto p : path) {
+        path_map[p.first][p.second] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) cout << path_map[i][j] << " ";
+        cout << endl;
+    }
 }
