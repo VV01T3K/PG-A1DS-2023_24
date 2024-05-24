76cffb467ba9d7e0bc96055170c3822dde179295
VV01T3K <wojteks.access@gmail.com>
2024-05-24 21:20:38 +0000

    cos

--- Project/Array.hpp
+++ Project/Array.hpp
@@ -1,26 +1,25 @@
 #pragma once
-#include <iostream>
-
 template <typename T>
 class Array {
    private:
     T* array;
-    size_t size;
+    size_t arraySize = 0;
 
    public:
-    Array(size_t size, const T& initVal = T()) : size(size) {
-        array = new T[size]{initVal};
-        std::fill_n(array, size, initVal);
+    Array() : array(nullptr) {}
+    Array(size_t size) : arraySize(size) { array = new T[size]; }
+    Array(size_t size, const T& initVal) : arraySize(size) {
+        array = new T[size];
+        for (size_t i = 0; i < size; ++i) array[i] = initVal;
     }
-
+    ~Array() { delete[] array; }
     T& operator[](size_t index) { return array[index]; }
     const T& operator[](size_t index) const { return array[index]; }
-    size_t size() const { return size; }
+    size_t size() const { return arraySize; }
     T* data() { return array; }
-    ~Array() { delete[] array; }
 
     T* begin() { return array; }
     const T* begin() const { return array; }
-    T* end() { return array + size; }
-    const T* end() const { return array + size; }
+    T* end() { return array + arraySize; }
+    const T* end() const { return array + arraySize; }
 };
---
+++ Stack.hpp
@@ -0,0 +1,20 @@
+#pragma once
+// !! danger !! this is a simplified version of the Stack container
+template <typename T>
+class Stack {
+   private:
+    T* array;
+    int size = 0;
+    int max_size;
+
+   public:
+    explicit Stack(int size) : max_size(size) { array = new T[size]; }
+    ~Stack() { delete[] array; }
+
+    void push(T element) {
+        if (size < max_size) array[size++] = element;
+    }
+    T pop() { return array[--size]; }
+    T& top() { return array[size - 1]; }
+    bool empty() const { return size == 0; }
+};
